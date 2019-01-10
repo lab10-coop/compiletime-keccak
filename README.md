511cc196f00c06cce387b440f1e7634c0924a4e3
davidf <dforsten@gmail.com>
2019-01-10 14:26:17 +0100

    Updated README and general cleanup

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-Procedural macro to calculate a keccak hash at compile-time and initialize a H256 for use at run-time.
+A procedural Rust macro to calculate a keccak hash at compile-time.
+
+Generates functions returning a H256 instance containing hash value baked at compile-time.
+
+An example test comparing a run-time calculated hash to the hash calculated at compile-time:
+
+```
+keccak_derive::compiletime_keccak!(hashed_string);
+
+#[test]
+fn compare_compile_time_to_runtime_keccak() {
+    let hash = keccak(b"hashed_string");
+    assert_eq!(hashed_string(), hash);
+}
+```
 
 Its main purpose at the time of writing is to avoid the overhead of repeatedly calculating a static keccak hash in environments where computation is expensive.
 Examples of such an environments are pwasm and ewasm smart contracts.
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
-#![recursion_limit="128"]
-
 extern crate proc_macro;
 use crate::proc_macro::{ TokenStream };
+use quote::quote;
 
 mod utils;
 
 #[proc_macro]
-pub fn make_compiletime_h256(item: TokenStream) -> TokenStream {
+pub fn compiletime_keccak(item: TokenStream) -> TokenStream {
     let name = syn::parse_macro_input!(item as syn::Ident);
     let hash = utils::keccak(name.to_string().as_bytes());
 
-    let all_vals: Vec<proc_macro2::TokenStream> = hash.as_ref().iter().map(|val| {
-        quote::quote! { #val, }
+    let hash_values: Vec<proc_macro2::TokenStream> = hash.as_ref().iter().map(|val| {
+        quote! { #val }
     }).collect();
 
-    let result = quote::quote! {
-        fn #name() -> H256 { H256::from([ #(#all_vals)* ]) }
+    let result = quote! {
+        fn #name() -> H256 { H256::from([ #(#hash_values),* ]) }
     };
     result.into()
 }
